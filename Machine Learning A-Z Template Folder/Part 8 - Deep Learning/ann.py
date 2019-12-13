cbf925ba84e170bca8248a2ab93467112ff17051
Silas Pereira Lima Filho <Silas.Pereira@ibm.com>
2019-12-13 17:53:55 -0300

    continue

---
+++ Machine Learning A-Z Template Folder/Part 8 - Deep Learning/ann.py
@@ -0,0 +1,77 @@
+#%%
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+#%%
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+#%%
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+#%%
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#%%
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+X_train
+
+#%%
+# Part 2 - Making the ANN
+# importing Keras and its modules
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#%%
+# initializing ANN
+classifier = Sequential()
+#%%
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim=6, init='uniform',
+                     activation='relu', input_dim=11))
+
+#%%
+# Adding the second hidden layer
+classifier.add(Dense(output_dim=6, init='uniform',
+                     activation='relu'))
+
+#%%
+# Adding the output layer
+classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+
+#%%
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+#%%
+# Predicting the test  set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+y_pred
+#%%
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+#%%
+cm
